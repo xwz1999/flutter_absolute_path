0f86647e07b97b28a02c9192201ee776efdfa097
章文轩 <12812285557@qq.com>
2022-12-05 11:38:14 +0800

    升级

--- example/android/app/src/main/kotlin/com/kasem/flutter_absolute_path_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/kasem/flutter_absolute_path_example/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.kasem.flutter_absolute_path_example
 
 import io.flutter.embedding.android.FlutterActivity
-
+import io.flutter.embedding.engine.FlutterEngine;
 class MainActivity: FlutterActivity() {
 }
--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/roaakhaddam/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/roaakhaddam/Documents/flutter-projects/flutter_absolute_path/example"
+export "FLUTTER_ROOT=/Users/datang/fvm/versions/2.8.1"
+export "FLUTTER_APPLICATION_PATH=/Users/datang/flutter_absolute_path/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
